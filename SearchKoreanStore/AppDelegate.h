dd8e8640c88a003cd9ba2539bd6e76abef08b879
Jake Choi <JakeChoi@Jakes-MacBook-Pro.local>
2013-09-11 14:25:16 +1000

    Initial Commit

---
+++ SearchKoreanStore/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  SearchKoreanStore
+//
+//  Created by Jake Choi on 11/09/13.
+//  Copyright (c) 2013 Jake Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
